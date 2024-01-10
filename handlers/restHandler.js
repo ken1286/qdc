73d6e4b89cea10c82e5ece1ceb9356f75db9cef8
ken1286 <ken.ridenour4@gmail.com>
2024-01-10 12:49:31 -0800

    removed handlers folder

--- handlers/restHandler.js
+++
@@ -1,9 +0,0 @@
-// const DeathController = require('../controllers/deathController');
-
-// class RestHandler {
-//   static deathCount(req, res) {
-//     res.status(200).json(DeathController.deathCount());
-//   }
-// }
-
-// module.exports = RestHandler;
